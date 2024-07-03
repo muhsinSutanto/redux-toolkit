cdbc9afbebd6b95cdfeec34f054e117fe4a45436
muhsinSutanto <sutantomuhsin@gmail.com>
2024-07-03 22:04:11 +0700

    add persist data

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
+//falback, kalo ada error/ loading
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<p>loading.....</p>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
+// localStorage.getItem("persist:menu")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterReducer";
 import counterReducers from "./features/counter/counterSlice";
-import menuReducer from "./features/menu/menuSlice";
+import menuReducers from "./features/menu/menuSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "menu",
+  storage,
+};
+
+const menuReducer = persistReducer(persistConfig, menuReducers);
 
 const store = configureStore({
   reducer: {
@@ -11,4 +20,6 @@ const store = configureStore({
   },
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
