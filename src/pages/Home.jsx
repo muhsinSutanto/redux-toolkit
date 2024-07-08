9281101784507eb064d4da5390de874935ede44f
muhsinSutanto <sutantomuhsin@gmail.com>
2024-07-08 21:31:41 +0700

    Revert "persist reducer"
    This reverts commit 673a1b90d751665f6a434e4dcf2560c93228ce5f.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { increment, decrement } from "../redux/features/counter/counterAction";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 
 function Home() {
-  const [image, setImage] = useState(null);
-
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counterReducer);
   const test = useSelector((state) => state.counter);
@@ -23,38 +20,7 @@ function Home() {
     console.log(count);
   }, [count]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append("slip", image);
-
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        access_token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY2NTI0MjUwOX0.ZTx8L1MqJ4Az8KzoeYU2S614EQPnqk6Owv03PUSnkzc",
-      },
-    };
-
-    const orderId = 5720;
-
-    try {
-      const response = await axios.put(
-        `https://api-car-rental.binaracademy.org/customer/order/${orderId}/slip`,
-        formData,
-        config
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log(error.response);
-    }
-  };
-
-  const handleImage = (e) => {
-    console.log(e.target.files[0]);
-    setImage(e.target.files[0]);
-  };
+  console.log("test", test);
   return (
     <>
       <h1> homepage</h1>
@@ -66,18 +32,6 @@ function Home() {
       )}
       <button onClick={handleAdd}>tambah</button>
       <button onClick={handleMinus}>kurang</button>
-
-      <div>
-        <input onChange={handleImage} type="file" />
-        <button onClick={handleSubmit}>submit</button>
-      </div>
-
-      <img src={image} alt="" />
-      <div>
-        <p>{test}</p>
-        <p>{menuReducer.menu.map((item) => item.name)}</p>
-        <p>{menuReducer.menu.map((item) => item.price)}</p>
-      </div>
     </>
   );
 }
